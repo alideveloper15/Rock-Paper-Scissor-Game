8d665e137261bce3193d8356f622f0ca0471af05
alideveloper15 <172058104+alideveloper15@users.noreply.github.com>
2025-06-23 21:51:13 +0500

    Added AutoPlay

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let score = JSON.parse(localStorage.getItem('score')) || {
-        wins:0,
-        losses:0,
-        ties:0
-    }
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
 
 updateScoreElement()
 
@@ -14,54 +14,71 @@ updateScoreElement()
     }
 }*/
 
-function playGame(playerMove){
+let isAutoPlaying = false
+let intervalId
+function autoPlay() {
+    if(!isAutoPlaying){
+        intervalId = setInterval(function () {
+        const playerMove = pickComputerMove()
+        playGame(playerMove)
+        }, 1000);
+        isAutoPlaying=true
+    }
+    else{
+        clearInterval(intervalId)
+        isAutoPlaying = false
+    }
+
+}
+
+function playGame(playerMove) {
     const computerMove = pickComputerMove();
     let result = '';
-    
-    if(playerMove==='scissors'){
-        if(computerMove === 'scissors'){
-        result = 'Tie.'
+
+    if (playerMove === 'scissors') {
+        if (computerMove === 'scissors') {
+            result = 'Tie.'
         }
-        else if(computerMove === 'rock'){
+        else if (computerMove === 'rock') {
             result = 'You lose.'
         }
-        else if(computerMove === 'paper'){
+        else if (computerMove === 'paper') {
             result = 'You win.'
         }
     }
 
-    else if(playerMove==='paper'){
-        if(computerMove === 'paper'){
+    else if (playerMove === 'paper') {
+        if (computerMove === 'paper') {
             result = 'Tie.'
         }
-        else if(computerMove === 'scissors'){
+        else if (computerMove === 'scissors') {
             result = 'You win.'
         }
-        else if(computerMove === 'rock'){
+        else if (computerMove === 'rock') {
             result = 'You lose.'
-        }
-    }
-    
-    else if(playerMove==='rock'){
-        if(computerMove === 'rock'){
-            result = 'Tie.'
-        }
-        else if(computerMove === 'paper'){
-            result = 'You lose.'
-        }
-        else if(computerMove === 'scissors'){
-        result = 'You win.'
         }
     }
 
-    if (result==='You win.'){
+    else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie.'
+        }
+        else if (computerMove === 'paper') {
+            result = 'You lose.'
+        }
+        else if (computerMove === 'scissors') {
+            result = 'You win.'
+        }
+    }
+
+    if (result === 'You win.') {
         score.wins += 1
     }
-    else if(result==='You lose.'){
-        score.losses+=1
+    else if (result === 'You lose.') {
+        score.losses += 1
     }
-    else if(result==='Tie.'){
-        score.ties+=1
+    else if (result === 'Tie.') {
+        score.ties += 1
     }
 
 
@@ -72,16 +89,16 @@ function playGame(playerMove){
     document.querySelector('.js-result').innerHTML = result
 
     document.querySelector('.js-moves')
-    .innerHTML= `You
+        .innerHTML = `You
         <img src="images/${playerMove}-emoji.png" alt="" class="move-icon">
         <img src="images/${computerMove}-emoji.png" alt="" class="move-icon">
         Computer`
 
 }
 
-function updateScoreElement(){
+function updateScoreElement() {
     document.querySelector('.js-score')
-        .innerHTML= `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+        .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
 }
 
 
@@ -89,13 +106,13 @@ function pickComputerMove() {
     const randomNumber = Math.random()
     let computerMove = '';
 
-    if(randomNumber >=0 && randomNumber < 1/3){
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
         computerMove = 'rock'
     }
-    else if(randomNumber >=1/3 && randomNumber < 2/3){
+    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
         computerMove = 'paper'
     }
-    else if(randomNumber >=2/3 && randomNumber <1){
+    else if (randomNumber >= 2 / 3 && randomNumber < 1) {
         computerMove = 'scissors'
     }
 
